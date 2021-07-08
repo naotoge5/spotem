571b6f0ac01f1970cfd3b6642a88605078d20cda
naotoge5 <10naotoge5.ykputi@gmail.com>
2021-07-08 11:29:54 +0900

    favicon update android-chrome add

--- home.php
+++ home.php
@@ -6,11 +6,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/public/bulma.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="android-chrome-192x192.png">
     <title>Document</title>
 </head>
-<?php echo $_SESSION['alert'];?>
+<?php echo $_SESSION['alert']; ?>
+
 <body>
-    
+
     <h1>home.php(timeline)</h1>
     <h1>home.php(timeline)</h1>
     <h1>home.php(timeline)</h1>
