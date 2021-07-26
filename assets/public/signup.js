85e14c08d77ea55d4fa555bae1b8430564e6fa16
naotoge5 <10naotoge5.ykputi@gmail.com>
2021-07-26 23:30:29 +0900

    7-26-23:30

--- assets/public/signup.js
+++ assets/public/signup.js
@@ -1,45 +1,65 @@
 auth_code = 0;
 $(function () {
-    $("#last").click(function(){
-        $("form").submit();
-    });
-    $(".next").click(function (e) {
-
-        if (e.target.id == "toSecond") {
-            $("#first").addClass("is-hidden");
-            $("#second").removeClass("is-hidden");
-        } else {
+    $("._next").click(function () {
+        if (!$("#__one").hasClass("is-hidden")) {
+            $("#__one, #__two, ._back").toggleClass("is-hidden");
+        } else if (!$("#__two").hasClass("is-hidden")) {
             sendAuthCode($("input[name='email']").val());
-            $("#second").addClass("is-hidden");
-            $("#third").removeClass("is-hidden");
+            $("#__two, #__three").toggleClass("is-hidden");
+        } else {
+            $("form").submit();
         }
     });
-    $("#first").change(function (e) {
+
+    $("._back").click(function () {
+        if (!$("#__two").hasClass("is-hidden")) {
+            $("#__one, #__two, ._back").toggleClass("is-hidden");
+        } else {
+            $("input[name='code']").val("");
+            $("#code .is-danger").text("");
+            $("#__three ._next").prop('disabled', true);
+            $("#code .is-right i, input[name='code']").removeClass("is-danger is-success");
+            $("input[name='code']").removeClass("fa-check");
+            $("#__two, #__three").toggleClass("is-hidden");
+        }
+    });
+
+    $("._back").hover(function () {
+        // over
+        $("._back").toggleClass("has-text-success has-text-success-dark");
+    }, function () {
+        // out
+        $("._back").toggleClass("has-text-success has-text-success-dark");
+    }
+    );
+
+    $("#__one").change(function (e) {
         $("input[name='" + e.target.name + "']").removeClass("is-danger is-success");
-        $("#" + e.target.name + " p").text("");
+        $("#" + e.target.name + " .is-danger").text("");
         $("#" + e.target.name + " .is-right i").removeClass("fa-check");
-        $("#first .next").prop('disabled', true);
+        $("#__one ._next").prop('disabled', true);
         if (e.target.value != 0) {
             if (e.target.name == 'userid') {
                 var userid = $("input[name='userid']").val();
                 if (userid.match(/[^A-Za-z0-9]+/)) {
                     $("input[name='userid']").addClass("is-danger");
-                    $("#userid p").text("特殊文字不可");
+                    $("#userid .is-danger").text("特殊文字不可");
                 } else {
                     find(e.target);
                 }
             } else {
                 $("input[name='name']").addClass("is-success");
                 $("#name .is-right i").addClass("fa-check");
-                if ($("input[name='userid']").hasClass("is-success")) $("#first .next").prop('disabled', false);
+                if ($("input[name='userid']").hasClass("is-success")) $("#__one ._next").prop('disabled', false);
             }
         }
     });
-    $("#second").change(function (e) {
+    
+    $("#__two").change(function (e) {
         $("input[name='" + e.target.name + "']").removeClass("is-danger is-success");
-        $("#" + e.target.name + " p").text("");
+        $("#" + e.target.name + " .is-danger").text("");
         $("#" + e.target.name + " .is-right i").removeClass("fa-check");
-        $("#second .next").prop('disabled', true);
+        $("#__two ._next").prop('disabled', true);
         if (e.target.value != 0) {
             if (e.target.name == 'email') {
                 var email = $("input[name='email']").val();
@@ -47,11 +67,11 @@ $(function () {
                     find(e.target);
                 } else {
                     $("input[name='email']").addClass("is-danger");
-                    $("#email p").text("形式が正しくありません");
+                    $("#email .is-danger").text("形式が正しくありません");
                 }
             } else if (e.target.name == 'password') {
                 $("input[name='password_check']").removeClass("is-danger is-success");
-                $("#password_check p").text("");
+                $("#password_check .is-danger").text("");
                 $("password_check .is-right i").removeClass("fa-check");
                 var password = $("input[name='password']").val();
                 var password_check = $("input[name='password_check']").val();
@@ -61,14 +81,14 @@ $(function () {
                     if (password == password_check) {
                         $("input[name='password_check']").addClass("is-success");
                         $("#password_check .is-right i").addClass("fa-check");
-                        if ($("input[name='email']").hasClass("is-success")) $("#second .next").prop('disabled', false);
+                        if ($("input[name='email']").hasClass("is-success")) $("#__two ._next").prop('disabled', false);
                     } else if (password_check.length > 7) {
                         $("input[name='password_check']").addClass("is-danger");
-                        $("#password_check p").text("パスワードが一致しません");
+                        $("#password_check .is-danger").text("パスワードが一致しません");
                     }
                 } else {
                     $("input[name='password']").addClass("is-danger");
-                    $("#password p").text("形式が正しくありません");
+                    $("#password .is-danger").text("形式が正しくありません");
                 }
 
             } else {
@@ -78,30 +98,30 @@ $(function () {
                     if (password == password_check) {
                         $("input[name='password_check']").addClass("is-success");
                         $("#password_check .is-right i").addClass("fa-check");
-                        if ($("input[name='email']").hasClass("is-success")) $("#second .next").prop('disabled', false);
+                        if ($("input[name='email']").hasClass("is-success")) $("#__two ._next").prop('disabled', false);
                     } else if (password_check.length > 7) {
                         $("input[name='password_check']").addClass("is-danger");
-                        $("#password_check p").text("パスワードが一致しません");
+                        $("#password_check .is-danger").text("パスワードが一致しません");
                     }
                 }
             }
 
         }
     });
-    $("#third").change(function () {
+    $("#__three").change(function () {
         $("input[name='code']").removeClass("is-danger is-success");
-        $("#code p").text("");
+        $("#code .is-danger").text("");
         $("#code .is-right i").removeClass("fa-check");
+        $("#__three ._next").prop('disabled', true);
         var code = $("input[name='code']").val();
         if (code.length > 0) {
             if (code == auth_code) {
                 $("input[name='code']").addClass("is-success");
                 $("#code .is-right i").addClass("fa-check");
-                $("input[name='code']").prop('disabled', true);
-                $("#last").prop('disabled', false);
+                $("#__three ._next").prop('disabled', false);
             } else {
                 $("input[name='code']").addClass("is-danger");
-                $("#code p").text("認証コードが正しくありません");
+                $("#code .is-danger").text("認証コードが正しくありません");
             }
         }
     });
@@ -117,16 +137,16 @@ $(function () {
             // 通信成功時のコールバック処理
             if (data) {
                 $("#" + target.name + " input[name='" + target.name + "']").addClass("is-danger");
-                $("#" + target.name + " p").text("既に使用されています。");
+                $("#" + target.name + " .is-danger").text("既に使用されています。");
             } else {//return 0 ユーザー使用可
                 $("#" + target.name + " input[name='" + target.name + "']").addClass("is-success");
                 $("#" + target.name + " .is-right i").addClass("fa-check");
                 switch (target.name) {
                     case 'userid':
-                        if ($("input[name='name']").hasClass("is-success")) $("#first .next").prop('disabled', false);
+                        if ($("input[name='name']").hasClass("is-success")) $("#__one ._next").prop('disabled', false);
                         break;
                     case 'email':
-                        if ($("input[name='password_check']").hasClass("is-success")) $("#second .next").prop('disabled', false);
+                        if ($("input[name='password_check']").hasClass("is-success")) $("#__two ._next").prop('disabled', false);
                         break;
                 }
             }
